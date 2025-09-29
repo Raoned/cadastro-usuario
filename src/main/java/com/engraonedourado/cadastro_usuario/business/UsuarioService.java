5d7ee852a515461a03475623a41d4acbed876c2b
Raone Dourado <raoned99@gmail.com>
2025-09-28 23:12:54 -0400

    First commit

---
+++ src/main/java/com/engraonedourado/cadastro_usuario/business/UsuarioService.java
@@ -0,0 +1,39 @@
+package com.engraonedourado.cadastro_usuario.business;
+
+import com.engraonedourado.cadastro_usuario.infrastructure.entity.Usuario;
+import com.engraonedourado.cadastro_usuario.infrastructure.repository.UsuarioRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+
+    private final UsuarioRepository repository;
+    public UsuarioService(UsuarioRepository repository) {
+        this.repository = repository;
+    }
+
+    public void  salvarUsuario(Usuario usuario){
+        repository.saveAndFlush(usuario);
+    }
+
+    public Usuario buscarUsuarioPorEmail(String email){
+        return repository.findByEmail(email).orElseThrow(
+                ()-> new RuntimeException("Email não encontrado")
+        );
+    }
+
+    public void deletarUsuarioPorEmail(String email){
+        repository.deleteByEmail(email);
+    }
+
+    public void atualizarUsuarioPorId(Integer Id, @org.jetbrains.annotations.NotNull Usuario usuario){
+        Usuario usuarioEntity = repository.findById(Id).orElseThrow(() -> new RuntimeException("Usuario nao encontrado"));
+        Usuario usuarioAtualizado = Usuario.builder()
+                .email(usuario.getEmail() != null ?
+                        usuario.getEmail() : usuarioEntity.getEmail())
+                .nome(usuario.getNome()!=null ?
+                        usuario.getNome() : usuarioEntity.getNome())
+                .id(usuarioEntity.getId())
+                .build();
+    }
+}
